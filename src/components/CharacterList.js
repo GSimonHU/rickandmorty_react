3e73f6cb00eb74a677e031d7dce72896c6db8f77
Simon Gabor <simon.gabor87hu@gmail.com>
2021-02-12 14:39:20 +0100

    Complete back-end for characters, data is loading from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,35 +1,32 @@
-import { useState } from "react";
+import { useHttp } from "../custom_hooks/http";
+
 import CharacterCard from "./CharacterCard";
 import "../style/Characters.css";
 const CharacterList = () => {
-    const [characters, setCharacters] = useState([
-        {
-            id: 1,
-            name: "Rick Sanchez",
-            image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-            url: "https://rickandmortyapi.com/api/character/1",
-        },
-        {
-            id: 2,
-            name: "Morty Smith",
-            image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-            url: "https://rickandmortyapi.com/api/character/1",
-        },
-        {
-            id: 3,
-            name: "Morty Smith",
-            image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-            url: "https://rickandmortyapi.com/api/character/1",
-        },
-    ]);
-
-    return (
-        <div className="cardContainer">
-            {characters.map((character) => (
-                <CharacterCard key={character.id} character={character} />
-            ))}
-        </div>
+    const [isLoading, data] = useHttp(
+        "https://rickandmortyapi.com/api/character",
+        []
     );
+
+    let characters = null;
+    if (data) {
+        characters = data.results;
+    }
+
+    let content = <div>Data is loading</div>;
+
+    if (!isLoading && characters) {
+        content = (
+            <div className="cardContainer">
+                {characters.map((character) => (
+                    <CharacterCard key={character.id} character={character} />
+                ))}
+            </div>
+        );
+    } else if (!isLoading && !characters) {
+        content = <p>Failed to fetch characters.</p>;
+    }
+    return content;
 };
 
 export default CharacterList;
